ccafa6785e59e20a9e713bde4adc6bba67b659f2
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-10-28 15:17:17 +0000

    Able to convert images to greyscale

--- filter/helpers.c
+++ filter/helpers.c
@@ -1,9 +1,19 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3);
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+        }
+    }
 }
 
 // Convert image to sepia
